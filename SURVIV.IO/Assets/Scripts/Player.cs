eea94d68024cc092bd9260530de3c2612c37e615
Chelle <michelleandrea.manabat@benilde.edu.ph>
2025-07-21 04:06:05 +0800

    Milestone 1
    Almost done. Looting mechanics still being implemented and fixing camera map bounds

---
+++ SURVIV.IO/Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 move;
+    private float threshold = 0.01f;
+
+    private Vector2 lastDir = Vector2.up;
+
+    public Joystick movementJoystick;
+    public Joystick directionJoystick;
+
+    public Rigidbody2D playerRb;
+
+    public float speed;
+
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ManagePlayerMovement();
+        ManageFacingDirection();
+
+        ScreenBounds();
+    }
+
+    private void ManagePlayerMovement()
+    {
+        move.x = movementJoystick.Horizontal;
+        move.y = movementJoystick.Vertical;
+
+        if (Mathf.Abs(move.x) < threshold) move.x = 0f;
+        if (Mathf.Abs(move.y) < threshold) move.y = 0f;
+
+        move = new Vector2(move.x, move.y);
+    }
+
+    private void ScreenBounds()
+    {
+        transform.position = new Vector3(Mathf.Clamp(
+            transform.position.x, -102f, 93f),
+            Mathf.Clamp(transform.position.y, -48f, 48f),
+            0f);
+    }
+
+    private void ManageFacingDirection()
+    {
+        float hAxis = directionJoystick.Horizontal;
+        float vAxis = directionJoystick.Vertical;
+
+        Vector2 inputDir = new Vector2(hAxis, vAxis);
+
+        if (inputDir.sqrMagnitude > threshold)
+        {
+            lastDir = inputDir.normalized;
+        }
+
+        float zAxis = Mathf.Atan2(lastDir.x, lastDir.y) * Mathf.Rad2Deg;
+
+        transform.eulerAngles = new Vector3(0, 0, -zAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + move * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Player collided with " + collision.gameObject.name);
+        }
+    }
+}
---
+++ SURVIV.IO/Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    private void Awake()
+    {
+        // Singleton
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+}
